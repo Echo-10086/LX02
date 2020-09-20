2df97ae7100de5620ea8318ef6d28504dadfccf4
Echo-10086 <71053135+Echo-10086@users.noreply.github.com>
2020-09-20 12:33:52 +0800

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Model
+{
+    public class UserInfo
+    {
+        public string userName { get; set; }
+        public string passWord { get; set; }
+        public string qq { get; set; }
+        public string email { get; set; }
+        public string type { get; set; }
+        public string userImg { get; set; }
+
+    }
+    public class UserInfoNo : UserInfo
+    {
+        public int num { get; set; }
+    }
+    public class Page
+    {
+        //pageindex写错了
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+    }
+}
